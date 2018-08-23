5e6b0d14dea937bb623cc17e94a976b5b6ccceef
shahad-mahmud <shahad9381@gmail.com>
2018-08-24 03:07:22 +0600

    first commit. Basic land page.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shahad Mahmud</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="/portfolio/css/style.css">
+</head>
+<body>
+	<div id="about">
+		
+		<div id="left_split">
+			<img src="/portfolio/res/round.png" > <br>
+			
+		</div>
+
+		<div id="right_split">
+			<div id="text">
+				<h1>Shahad Mahmud</h1> <br>
+				Rajshahi University of Engineering & Technology<br>
+				Web and Android applications
+			</div>
+		</div>
+
+	</div>
+	<div class="edu">
+		<div class="text">
+			dfdfsssssssssssssssssssss
+		</div>
+	</div>
+</body>
+</html>
