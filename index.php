1ff22bb0a2a5ef98ef4a64809c2c203b0410552b
shahad-mahmud <shahad9381@gmail.com>
2020-04-06 23:31:58 +0600

    portfolio recreation

--- index.php
+++ index.php
@@ -1,205 +1,74 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-	<title>Shahad Mahmud</title>
-	<link rel="shortcut icon" href="/portfolio/res/round.png" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="js/jquery-2.1.4.min.js"></script>
-	<script src="js/skill.bars.jquery.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- load the bootstraps -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+
+    <title>Welcome | Shahad Mahmud</title>
 </head>
+
 <body>
+    <div class="shadow-sm bg-white rounded">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">About <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                </div>
+            </div>
+        </nav>
+    </div>
 
-<script>
-function myFunction() {
-    var testDiv = document.getElementById("test");
-    document.getElementById("demo").innerHTML = testDiv.offsetTop;
-}
-</script>
-	<div class="progress-container">
-    	<div class="progress-bar" id="myBar"></div>
-  	</div>
+    <div class="container-fluid" style="margin-top: 8px;">
+        <!-- here the about me goes -->
+        <div class="container" id="section_about">
 
-	<div id="about">
-		
-		<div id="left_split">
-			<img src="/portfolio/res/round.png" > <br>
-			
-		</div>
+            <div class="row align-items-center">
+                <div class="col text-center">
+                    <!-- <span class="float-md-left"> -->
+                        <img src="res/re.png" class="img-fluid" alt="Shahad Mahmud" width="250px">
+                    <!-- </span> -->
+                </div>
+                <div class="col-md-7 text-center">
+                    <!-- <span class="float-md-right"> -->
+                        <!-- ------------------------------------- -->
+                        <h1>
+                            <a href="" class="typewrite" data-period="2000" data-type='[ "Hi! Im Shahad.", "I develop android apps.", "I Love to learn." ]'>
+                                <span class="wrap"></span>
+                            </a>
+                        </h1>   
+                        <!-- ------------------------------------ -->
+                        This is a huge column. This is a huge column. 
+                        This is a huge column. This is a huge column. 
+                        This is a huge column. This is a huge column. 
+                        This is a huge column. This is a huge column. 
+                        This is a huge column. This is a huge column. 
+                        This is a huge column. This is a huge column.
 
-		<div id="right_split">
-			<div id="text">
-				<h1>Shahad Mahmud</h1>
-				<p id="ruet">Rajshahi University of Engineering & Technology<br>
-				Web and Android applications Developer</p>
-			</div>
-
-		</div>
-	</div>
-
-
-	<div class="edu">
-		<div class="text">
-			<div style="font-size: 65px; text-align: center; margin: 30px 0; font-variant: small-caps; font-weight: bold;">
-				Education	
-			</div>
-
-			<div class="timeline" style="text-align: justify;">
-				<div class="container left">
-					<div class="content">
-						<h2 style="color: #263429;">2010</h2>
-						<h3 style="font-variant: small-caps;color: #8E2607; margin: 14px 0;">Junior School Certificate (JSC)</h3>
-						<p style="color: #000; font-variant: small-caps;">I attened JSC examination from <font style="font-weight: bold;">Dinajpur Zilla School</font> under <font style="font-weight: bold;">Dinajpur Eduction Board</font> and passed with a result of <font style="font-weight: bold;">GPA 5.00</font> in all the subjects.</p>
-					</div>
-				</div>
-
-				<div class="container right">
-					<div class="content">
-						<h2 style="color: #263429">2013</h2>
-						<h3 style="font-variant: small-caps;color: #8E2607;  margin: 14px 0;">Secondary School Certificate (SSC)</h3>
-						<p style="color: #000; font-variant: small-caps;">I attened SSC examination from <font style="font-weight: bold;">Dinajpur Zilla School</font> under <font style="font-weight: bold;">Dinajpur Eduction Board</font> and passed with a result of <font style="font-weight: bold;">GPA 5.00</font> in all the subjects.</p>
-					</div>
-				</div>
-
-				<div class="container left">
-					<div class="content">
-						<h2 style="color: #263429">2015</h2>
-						<h3 style="font-variant: small-caps;color: #8E2607;  margin: 14px 0;">Higher Secondary Certificate (HSC)</h3>
-						<p style="color: #000; font-variant: small-caps;">I attened HSC examination from <font style="font-weight: bold;">Dinajpur Government College</font> under <font style="font-weight: bold;">Dinajpur Eduction Board</font> and passed with a result of <font style="font-weight: bold;">GPA 5.00</font> in all the subjects.</p>
-					</div>
-				</div>
-
-				<div class="container right">
-					<div class="content">
-						<h2 style="color: #263429">Current</h2>
-						<h3 style="font-variant: small-caps;color: #8E2607;  margin: 14px 0;">Bachelor of Engineering (BE)</h3>
-						<p style="color: #000; font-variant: small-caps;">At present, I study at <font style="font-weight: bold;">Rajshahi University of Engineering and Technology</font> in the department of <font style="font-weight: bold;">Computer Science and Engineering</font>. It is expected that I'll complete my graduation within January of 2020.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                    <!-- </span> -->
+                </div>
+            </div>
 
 
-	<div class="skill" id="skill">
-
-		<div style="font-size: 65px; text-align: center; margin: 30px 0; font-variant: small-caps; font-weight: bold;">
-			Skills	
-		</div>
-
-<!--*************************************************************************************-->
-<!--*************************************************************************************-->
-		<link rel="stylesheet" type="text/css" href="css/barstyle.css">
-<!--*************************************************************************************-->
-<!--*************************************************************************************-->
-
-		<div class="wrapper">
-		    <div class="skillbar" data-percent="90">
-		      <span class="skillbar-title" style="background: #d35400;">HTML5</span>
-		      <p class="skillbar-bar" style="background: #e67e22;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-		    
-		    <div class="skillbar" data-percent="60">
-		      <span class="skillbar-title" style="background: #2980b9;">CSS3</span>
-		      <p class="skillbar-bar" style="background: #3498db;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-		    
-		    <div class="skillbar" data-percent="55">
-		      <span class="skillbar-title" style="background: #9F8F28;">Javascript</span>
-		      <p class="skillbar-bar" style="background: #D3BE35;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-		    
-		    <div class="skillbar" data-percent="80">
-		      <span class="skillbar-title" style="background: #46465e;">PHP</span>
-		      <p class="skillbar-bar" style="background: #5a68a5;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-
-		    <div class="skillbar" data-percent="75">
-		      <span class="skillbar-title" style="background: #87A953;">PL/SQL</span>
-		      <p class="skillbar-bar" style="background: #AFDA6C;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-
-		    <div class="skillbar" data-percent="35">
-		      <span class="skillbar-title" style="background: #9138B3;">Java</span>
-		      <p class="skillbar-bar" style="background: #BD4BE8;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-
-		    <div class="skillbar" data-percent="60">
-		      <span class="skillbar-title" style="background: #3AC3BB;">Algorithms</span>
-		      <p class="skillbar-bar" style="background: #45F3E9;"></p>
-		      <span class="skill-bar-percent"></span>
-		    </div>
-		    <!-- End Skill Bar -->
-		</div>
-		
-	</div>
+        </div>
+    </div>
 
 
-	<div class="footer">
-		<h1 style="margin: 8px; font-size: 60px;">Let's work together...</h1>
-		<button class="btn">Resume</button>
-		<div class="links">
-			<a href="https://web.facebook.com/shahad001" target="_blank"  class="fa fa-facebook"></a>
-			<a href="https://linkedin.com/in/shahad-mahmud" target="_blank" class="fa fa-linkedin"></a>
-			<a href="https://instagram.com/shahad_chowdhury" target="_blank" class="fa fa-instagram"></a>
-			<a href="https://github.com/shahad-mahmud" target="_blank" class="fa fa-github"></a>
-			<a href="mailto:shahad9381@gmail.com" target="_blank" class="fa fa-at"></a>
-		</div>
-	</div>
+    <!-- other js will go here -->
 
+    <!-- bootstraps essentials -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script src="js/custom.js"></script>
 </body>
-<script>
-	// When the user scrolls the page, execute myFunction 
-	window.onscroll = function() {
-		myFunction()
-		IsInView()
-	};
-
-	function myFunction() {
-		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-		var scrolled = (winScroll / height) * 100;
-		document.getElementById("myBar").style.height = scrolled + "%";
-	}
-
-	//the great (!!!) skill bar triger
-	var triggered = false;
-	//window.onscroll = function() { IsInView()	};
-
-	function IsInView(){
-		var skillDiv = document.getElementById("skill");
-		var skillDivHeight = skillDiv.offsetTop; //div's top offset
-		var top_position = document.documentElement.scrollTop; //Document offset on the top viewport
-		var window_heigth = window.innerHeight;
-		var animation_tigger = skillDivHeight - (window_heigth/1.2);
-		
-
-		if(top_position >= animation_tigger && !triggered)
-		{
-			$('.skillbar').skillBars({
-				from: 0,
-				speed: 3000, 
-				interval: 100,
-				decimals: 0,
-			});
-
-			triggered = true;
-		}
-
-		//console.log(animation_tigger);
-	}
-</script>
 </html>
---
+++ old_index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shahad Mahmud</title>
+	<link rel="shortcut icon" href="/portfolio/res/round.png" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/skill.bars.jquery.js"></script>
+</head>
+<body>
+
+<script>
+function myFunction() {
+    var testDiv = document.getElementById("test");
+    document.getElementById("demo").innerHTML = testDiv.offsetTop;
+}
+</script>
+	<div class="progress-container">
+    	<div class="progress-bar" id="myBar"></div>
+  	</div>
+
+  	
+
+	<div id="about">
+		
+		<div id="left_split">
+			<img src="/portfolio/res/round.png" > <br>
+			
+		</div>
+
+		<div id="right_split">
+			<div id="text">
+				<h1>Shahad Mahmud</h1>
+				<p id="ruet">Rajshahi University of Engineering & Technology<br>
+				Web and Android applications Developer</p>
+			</div>
+
+		</div>
+
+
+		<div class="arrow bounce" id="arrow">
+		    <div class="fa fa-arrow-down fa-2x" href="#"></div>
+		</div>
+
+
+	</div>
+
+
+	<div class="edu">
+		<div class="text">
+			<div style="font-size: 65px; text-align: center; margin: 30px 0; font-variant: small-caps; font-weight: bold;">
+				Education	
+			</div>
+
+			<div class="timeline" style="text-align: justify;">
+				<div class="container left">
+					<div class="content">
+						<h2 style="color: #263429;">2010</h2>
+						<h3 style="font-variant: small-caps;color: #8E2607; margin: 14px 0;">Junior School Certificate (JSC)</h3>
+						<p style="color: #000; font-variant: small-caps;">I attened JSC examination from <font style="font-weight: bold;">Dinajpur Zilla School</font> under <font style="font-weight: bold;">Dinajpur Eduction Board</font> and passed with a result of <font style="font-weight: bold;">GPA 5.00</font> in all the subjects.</p>
+					</div>
+				</div>
+
+				<div class="container right">
+					<div class="content">
+						<h2 style="color: #263429">2013</h2>
+						<h3 style="font-variant: small-caps;color: #8E2607;  margin: 14px 0;">Secondary School Certificate (SSC)</h3>
+						<p style="color: #000; font-variant: small-caps;">I attened SSC examination from <font style="font-weight: bold;">Dinajpur Zilla School</font> under <font style="font-weight: bold;">Dinajpur Eduction Board</font> and passed with a result of <font style="font-weight: bold;">GPA 5.00</font> in all the subjects.</p>
+					</div>
+				</div>
+
+				<div class="container left">
+					<div class="content">
+						<h2 style="color: #263429">2015</h2>
+						<h3 style="font-variant: small-caps;color: #8E2607;  margin: 14px 0;">Higher Secondary Certificate (HSC)</h3>
+						<p style="color: #000; font-variant: small-caps;">I attened HSC examination from <font style="font-weight: bold;">Dinajpur Government College</font> under <font style="font-weight: bold;">Dinajpur Eduction Board</font> and passed with a result of <font style="font-weight: bold;">GPA 5.00</font> in all the subjects.</p>
+					</div>
+				</div>
+
+				<div class="container right">
+					<div class="content">
+						<h2 style="color: #263429">Current</h2>
+						<h3 style="font-variant: small-caps;color: #8E2607;  margin: 14px 0;">Bachelor of Engineering (BE)</h3>
+						<p style="color: #000; font-variant: small-caps;">At present, I study at <font style="font-weight: bold;">Rajshahi University of Engineering and Technology</font> in the department of <font style="font-weight: bold;">Computer Science and Engineering</font>. It is expected that I'll complete my graduation within January of 2020.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="skill" id="skill">
+
+		<div style="font-size: 65px; text-align: center; margin: 30px 0; font-variant: small-caps; font-weight: bold;">
+			Skills	
+		</div>
+
+<!--*************************************************************************************-->
+<!--*************************************************************************************-->
+		<link rel="stylesheet" type="text/css" href="css/barstyle.css">
+<!--*************************************************************************************-->
+<!--*************************************************************************************-->
+
+		<div class="wrapper">
+		    <div class="skillbar" data-percent="90">
+		      <span class="skillbar-title" style="background: #d35400;">HTML5</span>
+		      <p class="skillbar-bar" style="background: #e67e22;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+		    
+		    <div class="skillbar" data-percent="60">
+		      <span class="skillbar-title" style="background: #2980b9;">CSS3</span>
+		      <p class="skillbar-bar" style="background: #3498db;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+		    
+		    <div class="skillbar" data-percent="55">
+		      <span class="skillbar-title" style="background: #9F8F28;">Javascript</span>
+		      <p class="skillbar-bar" style="background: #D3BE35;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+		    
+		    <div class="skillbar" data-percent="80">
+		      <span class="skillbar-title" style="background: #46465e;">PHP</span>
+		      <p class="skillbar-bar" style="background: #5a68a5;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+
+		    <div class="skillbar" data-percent="75">
+		      <span class="skillbar-title" style="background: #87A953;">PL/SQL</span>
+		      <p class="skillbar-bar" style="background: #AFDA6C;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+
+		    <div class="skillbar" data-percent="35">
+		      <span class="skillbar-title" style="background: #9138B3;">Java</span>
+		      <p class="skillbar-bar" style="background: #BD4BE8;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+
+		    <div class="skillbar" data-percent="60">
+		      <span class="skillbar-title" style="background: #3AC3BB;">Algorithms</span>
+		      <p class="skillbar-bar" style="background: #45F3E9;"></p>
+		      <span class="skill-bar-percent"></span>
+		    </div>
+		    <!-- End Skill Bar -->
+		</div>
+		
+	</div>
+
+
+	<div class="footer">
+		<h1 style="margin: 8px; font-size: 60px;">Let's Work Together...</h1>
+		<button class="btn">Resume</button>
+		<div class="links">
+			<a href="https://web.facebook.com/shahad001" target="_blank"  class="fa fa-facebook"></a>
+			<a href="https://linkedin.com/in/shahad-mahmud" target="_blank" class="fa fa-linkedin"></a>
+			<a href="https://instagram.com/shahad_chowdhury" target="_blank" class="fa fa-instagram"></a>
+			<a href="https://github.com/shahad-mahmud" target="_blank" class="fa fa-github"></a>
+			<a href="mailto:shahad9381@gmail.com" target="_blank" class="fa fa-at"></a>
+		</div>
+	</div>
+
+</body>
+<script>
+	// When the user scrolls the page, execute myFunction 
+	window.onscroll = function() {
+		myFunction()
+		IsInView()
+		theArow()
+	};
+
+	function myFunction() {
+		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+		var scrolled = (winScroll / height) * 100;
+		document.getElementById("myBar").style.height = scrolled + "%";
+	}
+
+	//the great (!!!) skill bar triger
+	var triggered = false;
+	//window.onscroll = function() { IsInView()	};
+
+	function IsInView(){
+		var skillDiv = document.getElementById("skill");
+		var skillDivHeight = skillDiv.offsetTop; //div's top offset
+		var top_position = document.documentElement.scrollTop; //Document offset on the top viewport
+		var window_heigth = window.innerHeight;
+		var animation_tigger = skillDivHeight - (window_heigth/1.2);
+		
+
+		if(top_position >= animation_tigger && !triggered)
+		{
+			$('.skillbar').skillBars({
+				from: 0,
+				speed: 3000, 
+				interval: 100,
+				decimals: 0,
+			});
+
+			triggered = true;
+		}
+
+		//console.log(animation_tigger);
+	}
+
+	function theArow(){
+		var about = document.getElementById("about");
+		var arrow = document.getElementById("arrow");
+		var aboutOffset = about.offsetTop;
+		var topOffset = document.documentElement.scrollTop;
+
+		//console.log(topOffset);
+
+		if(topOffset > 0)
+			arrow.style.display = "none";
+		else if(topOffset == 0){
+			arrow.style.display = "inline";
+			//console.log("adasd");
+		}
+	}
+</script>
+</html>
